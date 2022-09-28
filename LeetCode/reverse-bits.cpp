223e719739590d3d4b34eaa6895f11202b05c790
r0ck1n70sh <52280340+r0ck1n70sh@users.noreply.github.com>
2022-09-28 22:58:25 +0530

    update -- LEETCODE/reverse-bits.cpp

--- LeetCode/reverse-bits.cpp
+++ LeetCode/reverse-bits.cpp
@@ -1,1 +1,11 @@
-
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t res=0;
+        
+        for (int p=0; p<32; p++) {
+            if (n & (1L<<p)) res = res | 1L<<(32-(p+1));
+        }        
+        return res;
+    }
+};
